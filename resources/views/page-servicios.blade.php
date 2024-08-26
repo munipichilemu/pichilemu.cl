7e3d060be9f48e6e7779c17d4fff2427830a5921
Marco González Luengo <noquierouser@gmail.com>
2024-08-26 16:59:42 -0400

    Listar subpáginas en page-servicios.blade.php

---
+++ resources/views/page-servicios.blade.php
@@ -0,0 +1,39 @@
+@extends('page')
+
+@section('content')
+  @parent
+
+  <div class="container mx-auto pb-16 max-w-[56rem]">
+    @query([
+      'post_type' => 'page',
+      'post_parent' => get_the_ID(),
+      'orderby' => 'menu_order',
+      'order' => 'ASC',
+    ])
+
+    @hasposts
+    <h2 class="sr-only">Secciones</h2>
+
+    <ul class="sections-grid">
+      @posts
+      <li class="section-item">
+        <a href="@permalink" class="section-link">
+          @if(has_post_thumbnail())
+            <img src="@thumbnail('header-half', false)" alt="@title" class="section-image" role="img">
+          @else
+            <img src="{{ get_field('default_thumbnail', 'option')['sizes']['header-half'] }}" alt="@title"
+                 class="section-image"
+                 role="img">
+          @endif
+
+          <h3 class="section-title">@title</h3>
+        </a>
+      </li>
+      @endposts
+    </ul>
+    @endhasposts
+    @noposts
+    <p class="no-section-message">No se encontraron subpáginas.</p>
+    @endhasposts
+  </div>
+@endsection
