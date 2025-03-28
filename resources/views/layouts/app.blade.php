011554fb80e309d89c1403dc6db69ccd9a21f277
Marco González Luengo <463168+noquierouser@users.noreply.github.com>
2025-03-28 12:45:49 -0300

    fonts: Agregar Inter a las fuentes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css" rel="stylesheet"/>
   @php(do_action('get_header'))
   @php(wp_head())
 </head>
